68210b8a1ad9fdfbe9f466326d9039f2f743c311
Ben Fortuna <bfortuna@odecee.com.au>
2020-12-02 11:34:55 +1100

    Updated pypi config

--- setup.py
+++ setup.py
@@ -12,15 +12,18 @@ setup(
     },
     author='Ben Fortuna',
     author_email='fortuna@micronode.com',
-    description='A collection of Terraform-based blueprints',
+    description='A tool for managing Terraform blueprints',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/micronode/bedrock',
-    packages=find_packages("src"),
+    url='https://github.com/micronode/bedrock-cli',
+    # packages=find_packages("src"),
+    packages=find_packages(),
     setup_requires=['better-setuptools-git-version'],
     install_requires=[
-        'PyYAML>=5.1',
-        'docker>=3.7.2',
+        'PyYAML>=5.3.1',
+        'docker>=4.4.0',
+        'dockerpty',
+        'simple-term-menu'
     ],
     python_requires='>=3.7',
     entry_points={
